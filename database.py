ca8389b0c3453e843252ccac861e495efeae9e21
AymenFarhani <aymenfarhani28@gmail.com>
2025-03-15 20:51:00 +0100

    update database

--- database.py
+++ database.py
@@ -1,7 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-URL_DATABASE='postgresql://${username}:{password}@localhost:5432/{db_name}'
+username = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+db_name = os.getenv("POSTGRES_DB")
+
+URL_DATABASE='postgresql://{username}:{password}@localhost:5432/{db_name}'
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
